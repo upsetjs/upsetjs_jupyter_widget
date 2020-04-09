45e280a51b3cd3f56441a0c5d76af50bbc7f8db5
Samuel Gratzl <sam@sgratzl.com>
2020-04-09 22:16:52 +0200

    start adding options

--- src/widget.ts
+++ src/widget.ts
@@ -3,7 +3,7 @@
 import { DOMWidgetModel, DOMWidgetView, ISerializers } from '@jupyter-widgets/base';
 
 import { MODULE_NAME, MODULE_VERSION } from './version';
-import { renderUpSet } from '@upsetjs/bundle';
+import { renderUpSet, UpSetProps, ISetLike } from '@upsetjs/bundle';
 
 export class UpSetModel extends DOMWidgetModel {
   defaults() {
@@ -15,7 +15,6 @@ export class UpSetModel extends DOMWidgetModel {
       _view_name: UpSetModel.view_name,
       _view_module: UpSetModel.view_module,
       _view_module_version: UpSetModel.view_module_version,
-      value: 'Test',
     };
   }
 
@@ -34,16 +33,70 @@ export class UpSetModel extends DOMWidgetModel {
 
 export class UpSetView extends DOMWidgetView {
   render() {
-    this.value_changed();
     this.model.on('change:value', this.value_changed, this);
+    this.renderImpl();
+  }
+
+  private changeSelection = (s: ISetLike<any> | null) => {
+    this.model.set('value', s ? s.name : null);
+  };
+
+  private generateProps(): UpSetProps<any> {
+    const state = this.model.get_state(true);
+
+    const props: UpSetProps<any> = {
+      sets: [],
+      width: 300,
+      height: 300,
+    };
+
+    const toCamelCase = (v: string) => v.replace(/([-_]\w)/g, (g) => g[1].toUpperCase());
+
+    Object.keys(state).forEach((key) => {
+      let v = state[key];
+      if (key.startsWith('_') || ['layout', 'value'].includes(key) || v == null) {
+        return;
+      }
+      const propName = toCamelCase(key);
+      if (propName === 'fontSizes' || propName === 'combinations') {
+        const converted: any = {};
+        Object.keys(v).forEach((key) => {
+          converted[toCamelCase(key)] = (v as any)![key];
+        });
+        v = converted;
+      }
+      (props as any)[propName] = v;
+    });
+
+    // TODO convert sets, queries, combinations
+
+    if (state.mode === 'click') {
+      props.onClick = this.changeSelection;
+    } else {
+      props.onHover = this.changeSelection;
+    }
+
+    return props;
+  }
+
+  private renderImpl() {
+    const props = this.generateProps();
+    const bb = this.el.getBoundingClientRect();
+    if (!bb.width || !bb.height) {
+      requestAnimationFrame(() => {
+        const bb2 = this.el.getBoundingClientRect();
+        props.width = bb2.width || 600;
+        props.height = bb2.height || 400;
+        renderUpSet(this.el, props);
+      });
+      return;
+    }
+    props.width = bb.width;
+    props.height = bb.height;
+    renderUpSet(this.el, props);
   }
 
   value_changed() {
-    const props: any = this.model.get_state(true);
-    props.sets = [];
-    const bb = this.el.getBoundingClientRect();
-    props.width = bb.width || 300;
-    props.height = bb.height || 300;
-    renderUpSet(this.el, props);
+    this.renderImpl();
   }
 }
--- upsetjs_jupyter_widget/upset.py
+++ upsetjs_jupyter_widget/upset.py
@@ -4,14 +4,19 @@
 UpSet.js Jupyter Widget
 """
 
-from ipywidgets import ValueWidget, DOMWidget, register, widget_serialization, Layout
-from ipywidgets.widgets.trait_types import InstanceDict
-from traitlets import Unicode
+from ipywidgets import (
+    ValueWidget,
+    Layout,
+    DOMWidget,
+    register,
+    widget_serialization,
+    Layout,
+)
+from traitlets import default, Bool, Unicode, Enum, Dict, List, Int, Union, Float, Tuple
 from ._frontend import MODULE_NAME, MODULE_VERSION
 
 
-class MyLayout(Layout):
-    _model_module_version = Unicode("^3").tag(sync=True)
+_setlike = dict(name=Unicode(), elems=List(trait=Int(), default_value=[]))
 
 
 @register
@@ -26,9 +31,62 @@ class UpSetWidget(ValueWidget, DOMWidget):
     _view_module = Unicode(MODULE_NAME).tag(sync=True)
     _view_module_version = Unicode(MODULE_VERSION).tag(sync=True)
 
-    # layout = InstanceDict(MyLayout).tag(sync=True, **widget_serialization)
+    mode = Enum(("hover", "click"), default_value="hover").tag(sync=True)
+    padding = Float(None, allow_none=True).tag(sync=True)
+    bar_padding = Float(None, allow_none=True).tag(sync=True)
+    dot_padding = Float(None, allow_none=True).tag(sync=True)
+    width_ratios = Tuple(
+        Float(), Float(), Float(), default_value=(0.25, 0.1, 0.65)
+    ).tag(sync=True)
+    height_ratios = Tuple(Float(), Float(), default_value=(0.6, 0.4)).tag(sync=True)
 
-    value = Unicode("Test").tag(sync=True)
+    # # TODO data, sets
+
+    combinations = Dict(
+        traits=dict(
+            type=Enum(("intersection", "union"), default_value="intersection"),
+            min=Int(None, allow_none=True),
+            max=Int(None, allow_none=True),
+            empty=Bool(False),
+        ),
+        default_value=None,
+        allow_none=True,
+    ).tag(sync=True)
+
+    value = Union((Dict(traits=_setlike.copy()), List(Int(), default_value=[]))).tag(
+        sync=True
+    )
+    queries = List(Dict(), default_value=[]).tag(sync=True)
+
+    theme = Enum(("light", "dark"), default_value="light").tag(sync=True)
+    selection_color = Unicode(None, allow_none=True).tag(sync=True)
+    alternating_background_color = Unicode(None, allow_none=True).tag(sync=True)
+    color = Unicode(None, allow_none=True).tag(sync=True)
+    text_color = Unicode(None, allow_none=True).tag(sync=True)
+    hover_hint_color = Unicode(None, allow_none=True).tag(sync=True)
+    not_member_color = Unicode(None, allow_none=True).tag(sync=True)
+
+    bar_label_offset = Float(None, allow_none=True).tag(sync=True)
+    set_name_axis_offset = Float(None, allow_none=True).tag(sync=True)
+    combination_name_axis_offset = Float(None, allow_none=True).tag(sync=True)
+
+    query_legend = Bool(None, allow_none=True).tag(sync=True)
+    export_buttons = Bool(None, allow_none=True).tag(sync=True)
+    font_family = Unicode(None, allow_none=True).tag(sync=True)
+    font_sizes = Dict(
+        traits=dict(
+            chart_label=Unicode(None, allow_none=True),
+            axis_tick=Unicode(None, allow_none=True),
+            set_label=Unicode(None, allow_none=True),
+            bar_label=Unicode(None, allow_none=True),
+            legend=Unicode(None, allow_none=True),
+        )
+    ).tag(sync=True)
+    numeric_scale = Enum(("linear", "log"), default_value="linear").tag(sync=True)
+    band_scale = Enum(("band", "band2"), default_value="band").tag(sync=True)
+
+    set_name = Unicode(None, allow_none=True).tag(sync=True)
+    combination_name = Unicode(None, allow_none=True).tag(sync=True)
 
     @property
     def selection(self):
@@ -40,3 +98,7 @@ class UpSetWidget(ValueWidget, DOMWidget):
 
     def on_selection_changed(self, callback):
         self.observe(lambda evt: callback(evt.new), "value")
+
+    @default("layout")
+    def _default_layout(self):
+        return Layout(height="400px", align_self="stretch")
