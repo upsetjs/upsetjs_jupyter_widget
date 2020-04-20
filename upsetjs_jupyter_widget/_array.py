6d3eef5b9d7649d549274fbc2b67346b8500ffd4
Samuel Gratzl <sam@sgratzl.com>
2020-04-20 21:33:11 +0200

    implement compress_index_array in python

--- upsetjs_jupyter_widget/_array.py
+++ upsetjs_jupyter_widget/_array.py
@@ -5,4 +5,40 @@ import typing as t
 
 
 def compress_index_array(indices: t.Iterable[int]) -> t.Union[t.List[int], str]:
-    return list(indices)
+    """
+    compresses an indices array similar to a python range expression
+    """
+    elems: t.List[int] = list(indices)
+
+    if not elems:
+        return elems
+
+    encoded: t.List[str] = []
+
+    sub_slice: t.List[int] = []
+
+    def push():
+        if len(sub_slice) == 1:
+            encoded.append(str(sub_slice[0]))
+        elif len(sub_slice) == 2 and sub_slice[-1] < 10:
+            encoded.append(str(sub_slice[0]))
+            encoded.append(str(sub_slice[1]))
+        else:
+            encoded.append(f"{sub_slice[0]}+{len(sub_slice) - 1}")
+        return []
+
+    for j, index in enumerate(elems):
+        if j > 0:
+            expected = sub_slice[-1] + 1
+            if index != expected:
+                # slice break
+                sub_slice = push()
+        sub_slice.append(index)
+
+    push()
+
+    compressed = ",".join(encoded)
+
+    if len(compressed) < len(elems) * 0.6:
+        return compressed
+    return elems
