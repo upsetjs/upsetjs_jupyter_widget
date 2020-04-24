42fe2b49a40a10add7e61eab775f4b2683b1b732
Samuel Gratzl <sam@sgratzl.com>
2020-04-24 09:51:25 +0200

    fix license text and add dump

--- scripts/bump.js
+++ scripts/bump.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 function bumpFile(file, reg, replace) {
+  console.log('bumping', file);
   const desc = path.resolve(file);
   const content = fs.readFileSync(desc).toString();
   const newContent = content.replace(reg, replace);
