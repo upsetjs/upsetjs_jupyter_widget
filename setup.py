1ef7b1fce2f4417217b18988febe9e4a477b0a85
Samuel Gratzl <sam@sgratzl.com>
2020-04-28 15:49:08 +0200

    fix format

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open("README.md") as readme_file:
 # Ensure a valid python version
 ensure_python(">=3.6")
 
-pkg = json.load(pjoin(HERE, 'package.json'))
+pkg = json.load(pjoin(HERE, "package.json"))
 
 nb_path = pjoin(HERE, name, "nbextension", "static")
 lab_path = pjoin(HERE, name, "labextension")
@@ -58,56 +58,55 @@ cmdclass["jsdeps"] = combine_commands(
 
 
 if __name__ == "__main__":
-  setup(
-    name=name,
-    description=pkg["description"],
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    version=pkg["version"],
-    scripts=glob(pjoin("scripts", "*")),
-    cmdclass=cmdclass,
-    packages=find_packages(),
-    author=pkg["author"]["name"],
-    author_email=pkg["author"]["email"],
-    url=pkg["homepage"],
-    platforms="Linux, Mac OS X, Windows",
-    keywords=["Jupyter", "Widgets", "IPython"],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Intended Audience :: System Administrators",
-        "License :: Free For Home Use",
-        "License :: Free for non-commercial use",
-        "License :: Free For Educational Use"
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Framework :: IPython",
-        "Framework :: Jupyter",
-        "Topic :: Scientific/Engineering :: Information Analysis",
-        "Topic :: Scientific/Engineering :: Visualization",
-        "Typing :: Typed"
-    ],
-    include_package_data=True,
-    install_requires=["ipywidgets>=7.5.0",],
-    extras_require={
-        "test": ["pytest>=3.6", "pytest-cov", "nbval",],
-        "examples": [
-            # Any requirements for the examples to run
+    setup(
+        name=name,
+        description=pkg["description"],
+        long_description=readme,
+        long_description_content_type="text/markdown",
+        version=pkg["version"],
+        scripts=glob(pjoin("scripts", "*")),
+        cmdclass=cmdclass,
+        packages=find_packages(),
+        author=pkg["author"]["name"],
+        author_email=pkg["author"]["email"],
+        url=pkg["homepage"],
+        platforms="Linux, Mac OS X, Windows",
+        keywords=["Jupyter", "Widgets", "IPython"],
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: System Administrators",
+            "License :: Free For Home Use",
+            "License :: Free for non-commercial use",
+            "License :: Free For Educational Use" "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Framework :: IPython",
+            "Framework :: Jupyter",
+            "Topic :: Scientific/Engineering :: Information Analysis",
+            "Topic :: Scientific/Engineering :: Visualization",
+            "Typing :: Typed",
         ],
-        "docs": [
-            "sphinx>=1.5",
-            "recommonmark",
-            "sphinx_rtd_theme",
-            "nbsphinx>=0.2.13,<0.4.0",
-            "jupyter_sphinx",
-            "nbsphinx-link",
-            "pytest_check_links",
-            "pypandoc",
-        ],
-    },
-    entry_points={},
-  )
+        include_package_data=True,
+        install_requires=["ipywidgets>=7.5.0",],
+        extras_require={
+            "test": ["pytest>=3.6", "pytest-cov", "nbval",],
+            "examples": [
+                # Any requirements for the examples to run
+            ],
+            "docs": [
+                "sphinx>=1.5",
+                "recommonmark",
+                "sphinx_rtd_theme",
+                "nbsphinx>=0.2.13,<0.4.0",
+                "jupyter_sphinx",
+                "nbsphinx-link",
+                "pytest_check_links",
+                "pypandoc",
+            ],
+        },
+        entry_points={},
+    )
